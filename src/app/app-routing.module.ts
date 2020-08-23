3ac9b07b08502a38903bcbe191f75a0ab95756fa
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-22 23:20:08 -0500

    feat: creando los componentes de paginas del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PricingComponent } from './components/pages/pricing/pricing.component';
+import { ProtectedComponent } from './components/pages/protected/protected.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'protected', component: ProtectedComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PricingComponent } from './components/pages/pricing/pricing.component';
+import { ProtectedComponent } from './components/pages/protected/protected.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PricingComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
