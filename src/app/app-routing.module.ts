64242ce29934939a460f98f13828b9a6aea1cb1d
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-23 22:15:42 -0500

    feat: mostrando info de usuario en la pagina protegida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PricingComponent } from './components/pages/pricing/pricing.component';
 import { ProtectedComponent } from './components/pages/protected/protected.component';
 import { AuthorizeComponent } from './components/pages/authorize/authorize.component';
 import { AuthGuard } from './services/auth.guard';
+
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'pricing', component: PricingComponent },
--- src/app/components/pages/protected/protected.component.ts
+++ src/app/components/pages/protected/protected.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-protected',
   templateUrl: './protected.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ProtectedComponent implements OnInit {
+  constructor(public auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
